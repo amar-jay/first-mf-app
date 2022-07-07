33df6120e3c6c51c3f12eadcfd71be33eac7cfd1
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-08 02:38:49 +0300

    Reloadi

---
+++ ecomm/src/components/PDPContent.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useMatch } from "react-location";
+
+import { currency } from "../lib/products";
+import { addToCart, useLoggedIn } from "../lib/cart";
+
+import type { LocationGenerics } from "../router";
+
+export default function PDPContent() {
+  const loggedIn = useLoggedIn();
+  const { product } = useMatch<LocationGenerics>().data;
+
+  return (
+    <div className="grid grid-cols-2 gap-5">
+      <div>
+        <img src={product.image} alt={product.name} />
+      </div>
+      <div>
+        <div className="flex">
+          <h1 className="font-bold text-3xl flex-grow">{product.name}</h1>
+          <div className="font-bold text-3xl flex-end">
+            {currency.format(product.price)}
+          </div>
+        </div>
+        {loggedIn && (
+          <div className="text-right mt-2">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
+              onClick={() => addToCart(product.id)}
+              id={`addtocart_${product.id}`}
+            >
+              Add to Cart
+            </button>
+          </div>
+        )}
+        <div className="mt-10">{product.description}</div>
+        <div className="mt-10">{product.longDescription}</div>
+      </div>
+    </div>
+  );
+}
